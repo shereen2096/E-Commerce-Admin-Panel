d27948e5b1284b26d17062085d6128d1228a062b
727724eucs253-sudo <727724eucs253@skcet.ac.in>
2026-07-01 07:14:54 +0000

    e commerse project

---
+++ springapp/src/main/java/com/examly/springapp/ECommerceAdminPanelApplication.java
@@ -0,0 +1,13 @@
+package com.examly.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceAdminPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceAdminPanelApplication.class, args);
+	}
+
+}
---
+++ springapp/src/main/java/com/examly/springapp/config/CorsConfig.java
@@ -0,0 +1,5 @@
+package com.examly.springapp.config;
+
+public class CorsConfig {
+    
+}
